64a65239ad72ff643fcb7831e2e3eb06cec243a7
Medhat Elmasry <A00127241@ad.bcit.ca>
2017-09-26 19:37:44 -0700

    Add project files.

---
+++ NorthwindScaffold/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NorthwindScaffold.MvcApplication" Language="C#" %>
---
+++ NorthwindScaffold/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NorthwindScaffold.Startup))]
+namespace NorthwindScaffold
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
