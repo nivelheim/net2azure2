48035a2139a288c7c9f712ec3fcc5de7bb392cc2
Joseph Jung <A00918606@ad.bcit.ca>
2017-09-26 20:56:27 -0700

    second practice

---
+++ NorthwindScaffold/Models/Partials/Category.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace NorthwindScaffold.Models.Northwind
+{
+    [MetadataType(typeof(CategoryMetaData))]
+    public partial class Category { }
+
+    public class CategoryMetaData
+    {
+        [Required]
+        [Display(Name = "Category")]
+        public object CategoryName { get; set; }
+
+        [Required]
+        [StringLength(30, ErrorMessage = "The {0} must be between {2} and {1} characters.", MinimumLength = 5)]
+        public object Description { get; set; }
+    }
+}
